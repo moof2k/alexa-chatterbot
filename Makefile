cfead776eb5854f3f8b005c8686d50c17302f7ef
Robert Rose <rrose@perrito.local>
2015-10-18 18:04:15 -0700

    Better processing of romeo & juliet

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 all:
-	rm index.js
-	rm shakespeare.zip
+	rm -rf index.js
+	rm -rf shakespeare.zip
 	cp lambda.js index.js ; cat jsMegaHal.js >> index.js ; ./process.pl < shakespeare.txt >> index.js
 	zip shakespeare.zip index.js
 
--- process.pl
+++ process.pl
@@ -2,19 +2,53 @@
 
 print "var megahal = new jsMegaHal(4);\n\n";
 
+$text = "";
+
 while(<STDIN>) {
     $line = $_;
-    chomp($line);
-    $line =~ s/,$//;
-    $line =~ s/,//g;
-    $line =~ s/'//g;
-    $line =~ s/-/ /g;
-    $line =~ s/\.//g;
-    $line =~ s/;//g;
-    $line = lc($line);
 
-    if($line =~ / /) {
-        print "megahal.add(\"" . $line . "\");\n";   
-    }    
+    @words = split(/ /, $line);
+
+    # Skip if any word is all uppercase
+    $skip = 0;
+    foreach(@words) {
+        $word = $_;
+        $word =~ s/'s$//;
+        if ($word eq uc $word) {
+            $skip = 1;
+        }
+    }
+
+    if ($skip == 1) {
+        next;
+    }
+
+    chomp($line);
+    $line =~ s/-/ /g;
+
+    if ($line =~ /\ /) {
+        $text .= $line . " ";
+
+        if ($line =~ /\.$/) {
+            print "megahal.add(\"" . $text . "\");\n";
+            $text = "";
+        }
+        elsif ($line =~ /\,$/) {
+            print "megahal.add(\"" . $text . "\");\n";
+            $text = "";
+        }
+        elsif ($line =~ /\:$/) {
+            print "megahal.add(\"" . $text . "\");\n";
+            $text = "";
+        }
+        elsif ($line =~ /\!$/) {
+            print "megahal.add(\"" . $text . "\");\n";
+            $text = "";
+        }
+        elsif ($line =~ /\?/) {
+            print "megahal.add(\"" . $text . "\");\n";
+            $text = "";
+        }
+    }
 }
 
